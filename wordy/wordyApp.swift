5c24391e3d9c3b3108f67e2a3ed9b16578021aef
Abdulganiy <labdughaneeydev@gmail.com>
2025-12-08 15:47:15 +0100

    Initial Commit

---
+++ wordy/wordyApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordyApp.swift
+//  wordy
+//
+//  Created by ABDULGANIY LAWAL on 08/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct wordyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
